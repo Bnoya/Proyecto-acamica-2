c4437d47eec2b88634aa0b4f64cce2007b954246
benja noya <benjanoya@hotmail.com>
2021-06-11 20:49:57 -0300

    Gifs-in

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function startDarkMode() {
     var element = document.getElementById("dark-mode");
     element.classList.toggle("dark-mode");
 }
+
+
+const apiKey = 'O1ETr1fxsaxXqPfEced8hyndbec7c3C9';
+const gif = document.getElementById('gif')
+
+async function getGifTrending(){
+    let url=`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+    try {
+        const response= await fetch(url);
+        const info = await response.json();
+        console.log(info.data[0]);
+        for (let index = 0; index < 11; index++) {
+            const img = document.createElement('img');
+            img.src=info.data[index].images.original.url;
+            img.setAttribute("class", "gifs");
+            img.setAttribute("id", info.data[index].id);
+            gif.appendChild(img);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getGifTrending(); 
